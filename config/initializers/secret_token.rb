d6739e19741fe81243177f76b5cb1b605a52a649
zambrevishal <vishal.zambre@weboniselab.com>
2014-09-20 11:02:22 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocFinder::Application.config.secret_token = '21f5ef8f04b8c058bd40ffd3510e38b656cf7b166da78aab7c49e28d3a3919612ec7b1965450e84faeadbefb4b73c3b6289ac8c151f0238cf6913f3f5a5dd0da'
