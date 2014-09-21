101619f58be0e37eaeb0a27af2756e00733d2bf2
zambrevishal <vishal.zambre@weboniselab.com>
2014-09-21 06:41:39 +0530

    add map link

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ActiveRecord::Base
   attr_accessible :name, :display_name
-	has_many :user_roles
+	has_many :user_roles, dependent: :destroy
 	has_many :users, through: :user_roles
 end
--- lib/fake_data.rb
+++ lib/fake_data.rb
@@ -4,8 +4,8 @@ class FakeData
       doctor = Doctor.new(:name => Faker::Name.name, :date_of_birth => Time.now - Random.rand(50).years, :experience => (Time.now - Random.rand(25).years).to_date, :email => Faker::Internet.email("vishal#{i}"), :degree => DEGREE.sample, :expertise => SPECIALTIES.sample, :phone_no => Faker::Number.number(10), :fees => Faker::Number.number(3), :meta_keyword => META_KEY.sample(2).join(','))
       doctor.save
       doctor.add_user
-      limit = (Random.rand(5)+1)
-      area = Area.order("RAND(id)").limit(limit)
+      limit = rand(1..4)
+      area = Area.all.sample(limit)
       limit.times do |j|
         clinic = doctor.clinics.create(:name => Faker::Name.name,
           address1: area[j].name,
